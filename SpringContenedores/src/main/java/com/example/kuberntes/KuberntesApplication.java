27c4dd7ee6c1aeac4b10fc75ae041169b173f436
ing-berrocal <ingeniero.berrocal@hotmail.com>
2023-05-31 20:07:17 -0500

    Redis

---
+++ SpringContenedores/src/main/java/com/example/kuberntes/KuberntesApplication.java
@@ -0,0 +1,13 @@
+package com.example.kuberntes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuberntesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuberntesApplication.class, args);
+	}
+
+}
---
+++ SpringContenedores/src/test/java/com/example/kuberntes/KuberntesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kuberntes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuberntesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
